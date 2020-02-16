05e65d78b99cc5dfa17711c9a7ac4ba374fb01cb
Александр Зубрицкий <5998971@gmail.com>
2020-02-16 13:28:43 +0300

    сервис для авторизации

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,18 +1,23 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+
 import { AdminLayoutComponent } from './shared/components/admin-layout/admin-layout.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+
+import {AuthService} from "../shared/services/auth.service";
+import {SharedModule} from "./shared/shared.module";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    SharedModule,
     RouterModule.forChild([
       {
         path: '', component: AdminLayoutComponent, children: [
@@ -26,6 +31,7 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
     ])
   ],
   exports: [RouterModule],
+  providers: [AuthService],
   declarations: [AdminLayoutComponent, LoginPageComponent, DashboardPageComponent, CreatePageComponent, EditPageComponent]
 })
 export class AdminModule {
--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {User} from "../../shared/interfaces";
+import {AuthService} from "../../shared/services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
@@ -9,7 +11,9 @@ import {User} from "../../shared/interfaces";
 })
 export class LoginPageComponent implements OnInit {
   form: FormGroup
-  constructor() { }
+  constructor(
+      private auth: AuthService,
+      private router: Router) { }
 
   ngOnInit() {
     this.form = new FormGroup( {
@@ -31,5 +35,9 @@ export class LoginPageComponent implements OnInit {
       email: this.form.value.email,
       password: this.form.value.password
     }
+    this.auth.login(user).subscribe(() => {
+      this.form.reset()
+      this.router.navigate(['/admin', 'dashboard'] ) //редирект после успешной авторизации
+    })
   }
 }
