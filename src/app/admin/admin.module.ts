dc56bb68630de43df2bd606e64b32aafc4b962de
Александр Зубрицкий <5998971@gmail.com>
2020-02-18 09:52:44 +0300

    pipe для фильтрации списка постов

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,37 +3,42 @@ import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
-import { AdminLayoutComponent } from './shared/components/admin-layout/admin-layout.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
-import { CreatePageComponent } from './create-page/create-page.component';
-import { EditPageComponent } from './edit-page/edit-page.component';
-
-import {AuthService} from "../shared/services/auth.service";
+import {AdminLayoutComponent} from './shared/components/admin-layout/admin-layout.component';
+import {LoginPageComponent} from './login-page/login-page.component';
+import {DashboardPageComponent} from './dashboard-page/dashboard-page.component';
+import {CreatePageComponent} from './create-page/create-page.component';
+import {EditPageComponent} from './edit-page/edit-page.component';
 import {SharedModule} from "./shared/shared.module";
 import {AuthGuard} from "../shared/services/auth.guard";
+import {SearchPipe} from "./shared/search.pipe";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    SharedModule,
-    RouterModule.forChild([
-      {
-        path: '', component: AdminLayoutComponent, children: [
-          {path: '', redirectTo: 'admin/login', pathMatch: 'full'},
-          {path: 'login', component: LoginPageComponent},
-          {path: 'dashboard', component: DashboardPageComponent, canActivate: [AuthGuard]},
-          {path: 'create', component: CreatePageComponent, canActivate: [AuthGuard]},
-          {path: 'post/:id/edit', component: EditPageComponent, canActivate: [AuthGuard]}
-        ]
-      }
-    ])
-  ],
-  exports: [RouterModule],
-  providers: [AuthGuard],
-  declarations: [AdminLayoutComponent, LoginPageComponent, DashboardPageComponent, CreatePageComponent, EditPageComponent]
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        SharedModule,
+        RouterModule.forChild([
+            {
+                path: '', component: AdminLayoutComponent, children: [
+                    {path: '', redirectTo: 'admin/login', pathMatch: 'full'},
+                    {path: 'login', component: LoginPageComponent},
+                    {path: 'dashboard', component: DashboardPageComponent, canActivate: [AuthGuard]},
+                    {path: 'create', component: CreatePageComponent, canActivate: [AuthGuard]},
+                    {path: 'post/:id/edit', component: EditPageComponent, canActivate: [AuthGuard]}
+                ]
+            }
+        ])
+    ],
+    exports: [RouterModule],
+    providers: [AuthGuard],
+    declarations: [
+        AdminLayoutComponent,
+        LoginPageComponent,
+        DashboardPageComponent,
+        CreatePageComponent,
+        EditPageComponent,
+        SearchPipe]
 })
 export class AdminModule {
 
--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {PostsService} from "../shared/posts.service";
+import {Post} from "../../shared/interfaces";
+import {Subscription} from "rxjs";
 
 @Component({
-  selector: 'app-dashboard-page',
-  templateUrl: './dashboard-page.component.html',
-  styleUrls: ['./dashboard-page.component.scss']
+    selector: 'app-dashboard-page',
+    templateUrl: './dashboard-page.component.html',
+    styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
+    posts: Post[] = [];
+    pSub: Subscription;
+    searchStr = '';
 
-  constructor() { }
+    constructor(private postsService: PostsService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.pSub = this.postsService.getAll().subscribe(posts => {
+            this.posts = posts
+        })
+    }
+
+    remove(id: String) {
+
+    }
+
+    ngOnDestroy() {
+        if (this.pSub) {
+            this.pSub.unsubscribe()
+        }
+    }
+
 
 }
